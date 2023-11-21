81a2f49baf16aedb3e4ec5986830564cd48d7625
Gabriel Jesus <gabriel.jesus@neurotech.com.br>
2023-11-21 19:29:07 -0300

    feat(HYT-004): Test second time with feature

--- custom-hooks/install_hooks.sh
+++ custom-hooks/install_hooks.sh
@@ -4,7 +4,7 @@ files=($(ls | grep -v '\.sh$'))
 
 echo "Installing local git hooks..."
 
-# Adjust to work into previous path
+# Adjust to work into previous path into next version
 for file in "${files[@]}"; do
     cp "./${file}" "../.git/hooks/${file}"
     chmod +x "../.git/hooks/${file}"
