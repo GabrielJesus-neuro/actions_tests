143efa71ae5a274dfdd7eb12f51f664066c092b3
Gabriel Jesus <gabriel.jesus@neurotech.com.br>
2023-11-21 19:26:55 -0300

    fix(HYT-005): Test into fix option

--- custom-hooks/install_hooks.sh
+++ custom-hooks/install_hooks.sh
@@ -4,6 +4,7 @@ files=($(ls | grep -v '\.sh$'))
 
 echo "Installing local git hooks..."
 
+# Adjust to work into previous path
 for file in "${files[@]}"; do
     cp "./${file}" "../.git/hooks/${file}"
     chmod +x "../.git/hooks/${file}"
